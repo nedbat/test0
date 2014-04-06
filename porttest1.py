41bb581cff0a56c09b56a4bbbef4facd8016d032
Ned Batchelder <ned@nedbatchelder.com>
2014-04-06 15:21:39 -0400

    Use 2/3-compatible print

--- porttest1.py
+++ porttest1.py
@@ -2,8 +2,8 @@
 from portfolio1 import Portfolio
 
 p = Portfolio()
-print "Empty portfolio cost: %s" % p.cost()
+print("Empty portfolio cost: %s" % p.cost())
 p.buy("IBM", 100, 176.48)
-print "With 100 IBM @ 176.48: %s" % p.cost()
+print("With 100 IBM @ 176.48: %s" % p.cost())
 p.buy("HPQ", 100, 36.15)
-print "With 100 HPQ @ 36.15: %s" % p.cost()
+print("With 100 HPQ @ 36.15: %s" % p.cost())
--- porttest3_broken.py
+++ porttest3_broken.py
@@ -2,11 +2,11 @@
 from portfolio1 import Portfolio
 
 p = Portfolio()
-print "Empty portfolio cost: %s, should be 0.0" % p.cost()
+print("Empty portfolio cost: %s, should be 0.0" % p.cost())
 assert p.cost() == 0.0
 p.buy("IBM", 100, 176)
-print "With 100 IBM @ 176.48: %s, should be 17648.0" % p.cost()
+print("With 100 IBM @ 176.48: %s, should be 17648.0" % p.cost())
 assert p.cost() == 17648.0
 p.buy("HPQ", 100, 36.15)
-print "With 100 HPQ @ 36.15: %s, should be 21263.0" % p.cost()
+print("With 100 HPQ @ 36.15: %s, should be 21263.0" % p.cost())
 assert p.cost() == 21263.0
