c23fbe32f00ffbf659ea8eb4a64428a77d6b4add
Ned Batchelder <ned@nedbatchelder.com>
2014-04-09 18:15:37 -0400

    Put IBM price back to the usual

--- porttest2.py
+++ porttest2.py
@@ -3,7 +3,7 @@ from portfolio1 import Portfolio
 
 p = Portfolio()
 print "Empty portfolio cost: %s, should be 0.0" % p.cost()
-p.buy("IBM", 100, 176)
-print "With 100 IBM @ 176: %s, should be 17600.0" % p.cost()
+p.buy("IBM", 100, 176.48)
+print "With 100 IBM @ 176.48: %s, should be 17648.0" % p.cost()
 p.buy("HPQ", 100, 36.15)
 print "With 100 HPQ @ 36.15: %s, should be 21263.0" % p.cost()
--- porttest3_broken.py
+++ porttest3_broken.py
@@ -4,7 +4,7 @@ from portfolio1 import Portfolio
 p = Portfolio()
 print "Empty portfolio cost: %s, should be 0.0" % p.cost()
 assert p.cost() == 0.0
-p.buy("IBM", 100, 176)
+p.buy("IBM", 100, 176.48)
 print "With 100 IBM @ 176.48: %s, should be 17648.0" % p.cost()
 assert p.cost() == 17648.0
 p.buy("HPQ", 100, 36.15)
